4a9250d08c10001807c01a801b3122689551c1d7
ZZHZPP <1657795594@qq.com>
2019-05-27 14:11:27 +0800

    tj

---
+++ js/index.js
@@ -0,0 +1,40 @@
+lazyLoadInit({
+    coverColor:"white",
+    offsetBottom:0,
+    offsetTopm:0,
+    showTime:1000,
+    // onLoadBackStart:function(i,e){
+    //     console.log("onLoadBackStart:"+i);
+    // }
+    // ,onLoadBackEnd:function(i,e){
+    //     console.log("onLoadBackEnd:"+i);
+    //     $(this).removeAttr("data-lazy-src");
+    //
+    // }
+});
+
+// 判断滚轮滚动，显示不同样式导航
+$(document).ready(function() {
+    $(window).scroll(function(){
+        var scrollTopPos=$(document).scrollTop();
+        if(scrollTopPos>0){
+            $("#maiBgColorFixed").show();
+            $("#maiBgColor").hide();
+        }else{
+            $("#maiBgColorFixed").hide();
+            $("#maiBgColor").show();
+        }
+    });
+});
+
+
+//判断浏览器是否出现滚动条
+$(function () {
+    if (window.screen.availHeight >= document.body.clientHeight) {
+        $("footer").css({"position":"fixed","bottom":"0"});
+    } else {
+        $("footer").css("position","relative");
+    }
+});
+
+
